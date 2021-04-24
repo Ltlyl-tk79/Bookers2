16bd0d78e87d166382be3c3a155b5a097563dbc6
Ltlyl-tk79 <taku.805740@gmail.com>
2021-04-24 12:35:44 +0000

    [Finish]6-10

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,46 @@
+class UsersController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+  def index
+    @users = User.all
+    @user = current_user
+    @book = Book.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to user_path(current_user)
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:update] = "You have updated user successfully."
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '483f21de75e1ff986dced91fa61a0a3bf77b9f44e9f45131819adbf8ad7b9d93fbf5456cbaaefc9ae4d0b6e339437f5f1060da6cec2559fc436eb7d6239e3a8d'
